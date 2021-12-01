181bc284f2951af15a9cc59093ced1bea986bd99
Mosa Rabea <84705560+mosa-rabea@users.noreply.github.com>
2021-12-01 09:43:28 +0200

    Update Graph.java

--- data-structures/graph/app/src/main/java/graph/Graph.java
+++ data-structures/graph/app/src/main/java/graph/Graph.java
@@ -63,24 +63,6 @@ public class Graph<T> {
 
 
 
-//    ALGORITHM BreadthFirst(vertex)
-//    DECLARE nodes <-- new List()
-//    DECLARE breadth <-- new Queue()
-//    DECLARE visited <-- new Set()
-//
-//    breadth.Enqueue(vertex)
-//            visited.Add(vertex)
-//
-//            while (breadth is not empty)
-//    DECLARE front <-- breadth.Dequeue()
-//            nodes.Add(front)
-//
-//            for each child in front.Children
-//            if(child is not visited)
-//            visited.Add(child)
-//            breadth.Enqueue(child)
-//
-//            return nodes;
 
     public List<Node<T>> breadthFirst(Node<T> root){
         List<Node<T>> list = new ArrayList<>();
